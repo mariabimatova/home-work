c623782bd813544e82a0b68834940bb331d6bba6
Maria Bimatova <maria.bimatova@gmail.com>
2022-01-23 15:31:19 +0100

    file_hm_task1 modified

--- file/file_hw_task1.py
+++ file/file_hw_task1.py
@@ -21,20 +21,27 @@ cook_book = {}
 
 
 def read_course(f):
-    course_name = f.readline().strip()
-    cook_book[course_name] = []
-    print(course_name)
+    course_name = f.readline().strip
+    m = []
+    cook_book[course_name] = m
+
     ing_count = int(f.readline())
 
     for i in range(ing_count):
         line = f.readline().strip()
         list_line = line.split("|")
-        print(list_line)
+        ing_dict = {}
+        ing_dict.setdefault("ingredient_name", list_line[0])
+        ing_dict.setdefault("quantity", list_line[1])
+        ing_dict.setdefault("measure", list_line[2])
+        m.append(ing_dict)
+
+
+
+        # print(list_line)
         # chto to sdelatj nado tut, chtob pustoi spisok zapolnitj elementami
         # na kagdiy ingredient (storku iz faila) nado dobavitj 1 element spiska
         # i etot element budet tipa slovarj (dict)
-
-
 with open('file11.txt', encoding='utf-8') as file1:
     while True:
         read_course(file1)
