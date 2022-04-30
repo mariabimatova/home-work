4c3990734412cc78f48271f35bbfe8b0536ce2ff
Maria Bimatova <maria.bimatova@gmail.com>
2022-05-01 01:04:26 +0200

    modified select db

---
+++ sql/select_db.sql
@@ -0,0 +1,15 @@
+select * from ganri
+order by name desc;
+
+select * from ispolniteli;
+select * from albomi;
+select * from trek;
+select * from sbornik;
+select * from ganri_idispolniteli;
+select * from ispolniteli_albomi;
+
+select * from  trek_sbornik;
+
+
+
+
